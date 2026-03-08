f55e22c41a4f1125db59dbe742f361b761725b1f
sarour-jahan-Tanvir <tanvirislam1563@gmail.com>
2026-03-08 19:28:34 +0600

    updated spinner and created class for order add button

--- src/main/java/com/example/cafeteriamangement/ItemController.java
+++ src/main/java/com/example/cafeteriamangement/ItemController.java
@@ -1,11 +1,17 @@
 package com.example.cafeteriamangement;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 
-public class ItemController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemController implements Initializable {
     @FXML
     private ImageView img;
 
@@ -15,6 +21,9 @@ public class ItemController {
     @FXML
     private Label priceLabel;
 
+    @FXML
+    private Spinner<Integer> itemQuantity;
+
     private Item item;
     public void setData(Item item){
         this.item = item;
@@ -22,5 +31,13 @@ public class ItemController {
         priceLabel.setText(item.getPrice() + Main.Currency);
         Image image = new Image(getClass().getResourceAsStream(item.getImgsrc()));
         img.setImage(image);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<Integer> valueFactory =
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10);
+        valueFactory.setValue(1);
+        itemQuantity.setValueFactory(valueFactory);
     }
 }
